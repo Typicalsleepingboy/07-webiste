f5b18552902809ab67f1602d78f80f4f0b0268c7
bolulembang <mpluto772@gmail.com>
2024-03-17 06:41:03 +0700

    first push

---
+++ typ.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slick-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+        nextArrow: '<button type="button" class="slick-next">Next</button>'
+    });
+});
